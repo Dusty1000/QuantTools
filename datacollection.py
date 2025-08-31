6f7050ba0b0c48f4edcc48618400d1b500152ab5
Cormac Pickford <pickfordcormac@gmail.com>
2025-08-31 12:00:02 +0100

    trying to roganise data

--- datacollection.py
+++ datacollection.py
@@ -34,6 +34,12 @@ print(yf.Ticker('AMD').incomestmt)
 amd = pd.DataFrame(columns=['2024','2023','2022','2021'])
 amd.loc['Total Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Total Revenue'],yf.Ticker('AMD').incomestmt.iloc[:, 1]['Total Revenue'],yf.Ticker('AMD').incomestmt.iloc[:, 2]['Total Revenue'],yf.Ticker('AMD').incomestmt.iloc[:, 3]['Total Revenue']    ]
 amd.loc['Gross Profit'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Gross Profit'],yf.Ticker('AMD').incomestmt.iloc[:, 1]['Gross Profit'],yf.Ticker('AMD').incomestmt.iloc[:, 2]['Gross Profit'],yf.Ticker('AMD').incomestmt.iloc[:, 3]['Gross Profit']    ]
+amd.loc['Cost Of Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Cost Of Revenue'],yf.Ticker('AMD').incomestmt.iloc[:, 1]['Cost Of Revenue'],yf.Ticker('AMD').incomestmt.iloc[:, 2]['Cost Of Revenue'],yf.Ticker('AMD').incomestmt.iloc[:, 3]['Cost Of Revenue']    ]
+amd.loc['Research And Development'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Research And Development'],yf.Ticker('AMD').incomestmt.iloc[:, 1]['Research And Development'],yf.Ticker('AMD').incomestmt.iloc[:, 2]['Research And Development'],yf.Ticker('AMD').incomestmt.iloc[:, 3]['Research And Development']    ]
+amd.loc['Capital Expenditure'] = [yf.Ticker('AMD').cashflow.iloc[:,0]['Capital Expenditure'],yf.Ticker('AMD').cashflow.iloc[:,1]['Capital Expenditure'],yf.Ticker('AMD').cashflow.iloc[:,2]['Capital Expenditure'],yf.Ticker('AMD').cashflow.iloc[:,3]['Capital Expenditure']]
+amd.loc['Repayment Of Debt'] = [yf.Ticker('AMD').cashflow.iloc[:,0]['Repayment Of Debt'],yf.Ticker('AMD').cashflow.iloc[:,1]['Repayment Of Debt'],yf.Ticker('AMD').cashflow.iloc[:,2]['Repayment Of Debt'],yf.Ticker('AMD').cashflow.iloc[:,3]['Repayment Of Debt']]
+print(yf.Ticker('AMD').cashflow)
+a = yf.Ticker('AMD').cashflow
+a.to_csv('store.csv')
 print(amd)
 
-#im so fucking lost, all i did was add the error flag now it doesnt even have errors
