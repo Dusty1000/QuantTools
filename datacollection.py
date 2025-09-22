597e00ca3b32e7eae31441a235d83fc1e8bf124a
Cormac Pickford <pickfordcormac@gmail.com>
2025-09-22 17:35:20 +0100

    trying to roganise data

--- datacollection.py
+++ datacollection.py
@@ -7,9 +7,53 @@ import pandas as pd
 from config import *
 
 
-#data = yf.download(sp500b,start = start_date,end = end_date,auto_adjust='true')
+class ticker:
+        name = ''
+        historyRaw = []
+        financialsRaw = []
 
-#data = data['Close']
+        def __init__(self,each):
+            self.historyRaw = data[each].tolist()
+            temp = pd.DataFrame(columns=['2024', '2023', '2022', '2021'])
+            temp.loc['Total Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Total Revenue'],
+                                        yf.Ticker('AMD').incomestmt.iloc[:, 1]['Total Revenue'],
+                                        yf.Ticker('AMD').incomestmt.iloc[:, 2]['Total Revenue'],
+                                        yf.Ticker('AMD').incomestmt.iloc[:, 3]['Total Revenue']]
+            temp.loc['Gross Profit'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Gross Profit'],
+                                       yf.Ticker('AMD').incomestmt.iloc[:, 1]['Gross Profit'],
+                                       yf.Ticker('AMD').incomestmt.iloc[:, 2]['Gross Profit'],
+                                       yf.Ticker('AMD').incomestmt.iloc[:, 3]['Gross Profit']]
+            temp.loc['Cost Of Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Cost Of Revenue'],
+                                          yf.Ticker('AMD').incomestmt.iloc[:, 1]['Cost Of Revenue'],
+                                          yf.Ticker('AMD').incomestmt.iloc[:, 2]['Cost Of Revenue'],
+                                          yf.Ticker('AMD').incomestmt.iloc[:, 3]['Cost Of Revenue']]
+            temp.loc['Research And Development'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Research And Development'],
+                                                   yf.Ticker('AMD').incomestmt.iloc[:, 1]['Research And Development'],
+                                                   yf.Ticker('AMD').incomestmt.iloc[:, 2]['Research And Development'],
+                                                   yf.Ticker('AMD').incomestmt.iloc[:, 3]['Research And Development']]
+            temp.loc['Capital Expenditure'] = [yf.Ticker('AMD').cashflow.iloc[:, 0]['Capital Expenditure'],
+                                              yf.Ticker('AMD').cashflow.iloc[:, 1]['Capital Expenditure'],
+                                              yf.Ticker('AMD').cashflow.iloc[:, 2]['Capital Expenditure'],
+                                              yf.Ticker('AMD').cashflow.iloc[:, 3]['Capital Expenditure']]
+            temp.loc['Repayment Of Debt'] = [yf.Ticker('AMD').cashflow.iloc[:, 0]['Repayment Of Debt'],
+                                            yf.Ticker('AMD').cashflow.iloc[:, 1]['Repayment Of Debt'],
+                                            yf.Ticker('AMD').cashflow.iloc[:, 2]['Repayment Of Debt'],
+                                            yf.Ticker('AMD').cashflow.iloc[:, 3]['Repayment Of Debt']]
+
+
+
+
+
+        def history(self,day):
+            return self.historyRaw[day]
+
+        def financials(self,day,feature):
+            index = findict[feature]
+            return self.financialsRaw[day][index]
+
+data = yf.download(sp500b,start = start_date,end = end_date,auto_adjust='true')
+
+data = data['Close']
 
 #print(data['A'].loc[data.index[0]])
 sto = pd.DataFrame([yf.Ticker('EBAY').cashflow['2024-12-31'] , yf.Ticker('EBAY').financials['2024-12-31']])
@@ -38,8 +82,63 @@ amd.loc['Cost Of Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Cost Of Re
 amd.loc['Research And Development'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Research And Development'],yf.Ticker('AMD').incomestmt.iloc[:, 1]['Research And Development'],yf.Ticker('AMD').incomestmt.iloc[:, 2]['Research And Development'],yf.Ticker('AMD').incomestmt.iloc[:, 3]['Research And Development']    ]
 amd.loc['Capital Expenditure'] = [yf.Ticker('AMD').cashflow.iloc[:,0]['Capital Expenditure'],yf.Ticker('AMD').cashflow.iloc[:,1]['Capital Expenditure'],yf.Ticker('AMD').cashflow.iloc[:,2]['Capital Expenditure'],yf.Ticker('AMD').cashflow.iloc[:,3]['Capital Expenditure']]
 amd.loc['Repayment Of Debt'] = [yf.Ticker('AMD').cashflow.iloc[:,0]['Repayment Of Debt'],yf.Ticker('AMD').cashflow.iloc[:,1]['Repayment Of Debt'],yf.Ticker('AMD').cashflow.iloc[:,2]['Repayment Of Debt'],yf.Ticker('AMD').cashflow.iloc[:,3]['Repayment Of Debt']]
+
 print(yf.Ticker('AMD').cashflow)
 a = yf.Ticker('AMD').cashflow
 a.to_csv('store.csv')
 print(amd)
+
+#for tickero in universe:
+        # amd.loc['Total Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Total Revenue'],
+        #                             yf.Ticker('AMD').incomestmt.iloc[:, 1]['Total Revenue'],
+        #                             yf.Ticker('AMD').incomestmt.iloc[:, 2]['Total Revenue'],
+        #                             yf.Ticker('AMD').incomestmt.iloc[:, 3]['Total Revenue']]
+        # amd.loc['Gross Profit'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Gross Profit'],
+        #                            yf.Ticker('AMD').incomestmt.iloc[:, 1]['Gross Profit'],
+        #                            yf.Ticker('AMD').incomestmt.iloc[:, 2]['Gross Profit'],
+        #                            yf.Ticker('AMD').incomestmt.iloc[:, 3]['Gross Profit']]
+        # amd.loc['Cost Of Revenue'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Cost Of Revenue'],
+        #                               yf.Ticker('AMD').incomestmt.iloc[:, 1]['Cost Of Revenue'],
+        #                               yf.Ticker('AMD').incomestmt.iloc[:, 2]['Cost Of Revenue'],
+        #                               yf.Ticker('AMD').incomestmt.iloc[:, 3]['Cost Of Revenue']]
+        # amd.loc['Research And Development'] = [yf.Ticker('AMD').incomestmt.iloc[:, 0]['Research And Development'],
+        #                                        yf.Ticker('AMD').incomestmt.iloc[:, 1]['Research And Development'],
+        #                                        yf.Ticker('AMD').incomestmt.iloc[:, 2]['Research And Development'],
+        #                                        yf.Ticker('AMD').incomestmt.iloc[:, 3]['Research And Development']]
+        # amd.loc['Capital Expenditure'] = [yf.Ticker('AMD').cashflow.iloc[:, 0]['Capital Expenditure'],
+        #                                   yf.Ticker('AMD').cashflow.iloc[:, 1]['Capital Expenditure'],
+        #                                   yf.Ticker('AMD').cashflow.iloc[:, 2]['Capital Expenditure'],
+        #                                   yf.Ticker('AMD').cashflow.iloc[:, 3]['Capital Expenditure']]
+        # amd.loc['Repayment Of Debt'] = [yf.Ticker('AMD').cashflow.iloc[:, 0]['Repayment Of Debt'],
+        #                                 yf.Ticker('AMD').cashflow.iloc[:, 1]['Repayment Of Debt'],
+        #                                 yf.Ticker('AMD').cashflow.iloc[:, 2]['Repayment Of Debt'],
+        #                                 yf.Ticker('AMD').cashflow.iloc[:, 3]['Repayment Of Debt']]
+findict = {
+    'Total Revenue' : 0,
+    'Gross Profit' : 1,
+    'Cost Of Revenue' : 2,
+    'Research And Development' : 3,
+    'Capital Expenditure' : 4,
+    'Repayment Of Debt' : 5,
+
+}
+
+everything = []
+for each in universe:
+    everything.append(ticker(each))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
